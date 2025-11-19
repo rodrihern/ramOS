8d12da6f762d69b2cc51350c22283b52e03a5fbc
Rodrigo Alejandro Hernandez <rohernandez003@gmail.com>
2025-11-18 23:53:07 -0300

    removed buddy mm and updated compilation script

--- compile.sh
+++ compile.sh
@@ -1,41 +1,17 @@
-
-if [ "$1" == "buddy" ]; then
-    MM="USE_BUDDY"
-else
-    MM=""
-fi
-
-# Name of the Docker container
 CONTAINER_NAME="ramOS"
 
-# Start the Docker container
 docker start $CONTAINER_NAME
-
-# Clean and build the project in the specified directories
-docker exec -it $CONTAINER_NAME make -C /root/Image clean
-docker exec -it $CONTAINER_NAME make -C /root/Image all
-
-# Clean and build the project in the specified directories
-docker exec -it $CONTAINER_NAME make -C /root/Toolchain clean
-docker exec -it $CONTAINER_NAME make -C /root/Toolchain all MM="$MM"
-MAKE_ROOT_EXIT_CODE=$?
-
-# Execute the make commands
-docker exec -it $CONTAINER_NAME make -C /root clean
-docker exec -it $CONTAINER_NAME make -C /root all  MM="$MM"
-MAKE_TOOLCHAIN_EXIT_CODE=$?
-
+docker exec -it $CONTAINER_NAME make -C /root
+MAKE_EXIT_CODE=$?
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' 
 
-# Check if both make commands were successful
-if [[ $MAKE_TOOLCHAIN_EXIT_CODE -eq 0 && $MAKE_ROOT_EXIT_CODE -eq 0 ]]; then
+# Check if make command was successful
+if [[ $MAKE_EXIT_CODE -eq 0 ]]; then
     echo -e "${GREEN}Compilation successful!${NC}"
     echo -e "${GREEN}Run './run.sh' to start the kernel${NC}"
 else
     echo -e "${RED}Compilation failed!${NC}"
 fi
-
-# docker stop $CONTAINER_NAME
